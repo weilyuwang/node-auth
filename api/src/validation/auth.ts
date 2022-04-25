e2a90033b3f686ddab77b2ffd4eced5e81221af4
weilyuwang <weilyuwang@gmail.com>
2022-04-25 19:40:50 -0400

    add comment

--- api/src/validation/auth.ts
+++ api/src/validation/auth.ts
@@ -4,6 +4,6 @@ import { BCRYPT_MAX_BYTES } from "../config";
 export const registerSchema = Joi.object({
     email: Joi.string().email().min(8).max(254).lowercase().trim().required(),
     name: Joi.string().min(3).max(128).trim().required(),
-    password: Joi.string().min(8).max(BCRYPT_MAX_BYTES, "utf8").required(),
+    password: Joi.string().min(8).max(BCRYPT_MAX_BYTES, "utf8").required(), // maximum 72 bytes
     passwordConfirmation: Joi.valid(Joi.ref("password")).required(),
 });
