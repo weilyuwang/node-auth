38336adc42090b638d2b8bcdd48fa4d73e276702
weilyuwang <weilyuwang@gmail.com>
2022-04-24 21:10:23 -0400

    use guest middleware

--- api/src/routes/register.ts
+++ api/src/routes/register.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { logIn } from "../auth";
+import { guest } from "../middleware";
 import { User } from "../models";
 import { registerSchema } from "../validation";
 const router = Router();
 
-router.post("/register", async (req, res) => {
+router.post("/register", guest, async (req, res) => {
     await registerSchema.validateAsync(req.body, { abortEarly: false });
     const { email, name, password } = req.body;
 
