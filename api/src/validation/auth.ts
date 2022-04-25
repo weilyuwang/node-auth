9da5cbe09172de8086d0b24e99131af1e374b2ad
weilyuwang <weilyuwang@gmail.com>
2022-04-25 19:48:54 -0400

    add regex constraint to password

--- api/src/validation/auth.ts
+++ api/src/validation/auth.ts
@@ -4,6 +4,13 @@ import { BCRYPT_MAX_BYTES } from "../config";
 export const registerSchema = Joi.object({
     email: Joi.string().email().min(8).max(254).lowercase().trim().required(),
     name: Joi.string().min(3).max(128).trim().required(),
-    password: Joi.string().min(8).max(BCRYPT_MAX_BYTES, "utf8").required(), // maximum 72 bytes
+    password: Joi.string()
+        .min(8)
+        .max(BCRYPT_MAX_BYTES, "utf8")
+        .regex(/^(?=.*?[\p{Lu}])(?=.*?[\p{Ll}])(?=.*?\d).*$/u)
+        .message(
+            '"{#label}" must contain one uppercase letter, one lowercase letter, and one digit'
+        )
+        .required(),
     passwordConfirmation: Joi.valid(Joi.ref("password")).required(),
 });
