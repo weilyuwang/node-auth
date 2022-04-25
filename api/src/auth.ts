2e313d019ea13aa62867b25a3c764ea8aa20bb43
weilyuwang <weilyuwang@gmail.com>
2022-04-24 21:08:23 -0400

    add guest middleware

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -3,3 +3,5 @@ import { Request } from "express";
 export const logIn = (req: Request, userId: string) => {
     req.session.userId = userId;
 };
+
+export const isLoggedIn = (req: Request) => !!req.session.userId;
