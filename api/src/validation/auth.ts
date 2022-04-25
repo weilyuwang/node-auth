6fc7ffe422eb04c55f617cd82b24356d4102f8ad
weilyuwang <weilyuwang@gmail.com>
2022-04-25 19:38:14 -0400

    update password length constraint

--- api/src/validation/auth.ts
+++ api/src/validation/auth.ts
@@ -1,8 +1,9 @@
 import Joi from "joi";
+import { BCRYPT_MAX_BYTES } from "../config";
 
 export const registerSchema = Joi.object({
     email: Joi.string().email().min(8).max(254).lowercase().trim().required(),
     name: Joi.string().min(3).max(128).trim().required(),
-    password: Joi.string().min(3).max(128).required(), // TODO: max
+    password: Joi.string().min(8).max(BCRYPT_MAX_BYTES, "utf8").required(),
     passwordConfirmation: Joi.valid(Joi.ref("password")).required(),
 });
